1546b2f913d82090f0fab51df6e6a87101f86563
vantaber <134776983+vantaber@users.noreply.github.com>
2023-08-21 19:35:28 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,48 @@
-# -*- coding: cp1251 -*-
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
-from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.textinput import TextInput
 
 
-class CounterApp(App):
+class MainApp(App):
     def build(self):
-        self.count = 0
+        main_layout = BoxLayout(orientation="vertical", padding=10, spacing=10)
+        self.solution = TextInput(multiline=False, readonly=False, halign="right", font_size=55)
+        main_layout.add_widget(self.solution)
+        buttons = [
+            ["7", "8", "9", "/"],
+            ["4", "5", "6", "*"],
+            ["1", "2", "3", "-"],
+            [".", "0", "C", "+"],
+        ]
+        for row in buttons:
+            h_layout = BoxLayout()
+            for label in row:
+                button = Button(text=label, pos_hint={"center_x": 0.5, "center_y": 0.5})
+                button.bind(on_press=self.on_button_press)
+                h_layout.add_widget(button)
+            main_layout.add_widget(h_layout)
 
-        layout = BoxLayout(orientation='vertical')
-        self.label = Label(text='0', font_size=72)
-        button = Button(text='Увеличить', font_size=24)
-        button.bind(on_press=self.increment_count)
+        equals_button = Button(text="=", pos_hint={"center_x": 0.5, "center_y": 0.5})
+        equals_button.bind(on_press=self.on_solution)
+        main_layout.add_widget(equals_button)
 
-        layout.add_widget(self.label)
-        layout.add_widget(button)
+        # return a Button() as a root widget
+        return main_layout
 
-        return layout
+    def on_button_press(self, instance):
+        if instance.text == "C":
+            self.solution.text = ""
+        else:
+            self.solution.text += instance.text
 
-    def increment_count(self, instance):
-        self.count += 1
-        self.label.text = str(self.count)
+    def on_solution(self, instance):
+        if self.solution.text:
+            try:
+                self.solution.text = str(eval(self.solution.text))
+            except:
+                self.solution.text = "Error"
 
 
 if __name__ == '__main__':
-    CounterApp().run()
+    MainApp().run()
